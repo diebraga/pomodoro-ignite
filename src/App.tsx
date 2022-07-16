88b03fb1675b06a5bcdf549206f791ad6948cd69
Diego Braga <diegobraga@hertz.com>
2022-07-16 17:55:10 +0100

    router configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from "./components/Button"
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button variant="success"/>
-      <Button variant="danger"/>
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
