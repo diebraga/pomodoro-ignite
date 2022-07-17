24a17d25cac0a1c575b4b73f92dd831d55a3b450
Diego Braga <diegobraga@hertz.com>
2022-07-17 13:38:21 +0100

    styling countdown

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,10 +9,10 @@ export default function Header() {
     <HeaderContainer>
       <img src={Logo} alt="" />
       <nav>
-        <NavLink to="/" title='Timer'>
+        <NavLink to="/" title="Timer">
           <Timer size={24} />
         </NavLink>
-        <NavLink to="/history" title='History'>
+        <NavLink to="/history" title="History">
           <Scroll size={24} />
         </NavLink>
       </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,87 @@
+import { Play } from 'phosphor-react'
 import React from 'react'
+import styled from 'styled-components'
 
 export function Home() {
-  return <div>Home</div>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">{"I'm Going to work on"}</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="for">For</label>
+          <input type="number" id="for" />
+
+          <span>Minutes</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Divider>:</Divider>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Start
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
+
+const HomeContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(p) => p.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+const CountDownContainer = styled.div`
+  font-family: 'Roboto Mono' monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: ${(p) => p.theme['gray-100']};
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${(p) => p.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+const Divider = styled.div`
+  padding: 2rem 0;
+  color: ${(p) => p.theme['green-500']};
+
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`
